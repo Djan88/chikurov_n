dece9b780fbd079eca2e76cd7e006f5815ffc1a4
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-26 19:54:23 +0300

    release_13

--- wp-content/themes/chikurov_2020/front-page.php
+++ wp-content/themes/chikurov_2020/front-page.php
@@ -121,7 +121,7 @@
               <span class="parogramm_item_link btn btn-success btn-sm">Узнать подробнее</span>
             </span>
           </a>
-          <a href="/korrekciya-vnutrikostnyx-disfunkcij/" class="col-md-4 col-sm-6 col-xs-12 programm_item programm_item_3 hidden">
+          <a href="/korrekciya-vnutrikostnyx-disfunkcij-page/" class="col-md-4 col-sm-6 col-xs-12 programm_item programm_item_3 hidden">
             <span class="programm_item_inner">
               <span class="parogramm_item_heading">Коррекция внутрикостных дисфункций</span></br>
               <span class="parogramm_item_content">8 учебных часов</span></br>
