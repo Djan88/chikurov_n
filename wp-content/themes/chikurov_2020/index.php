fd5ed90442d25ec7f31f478004f38cec1aa543f7
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-28 19:59:30 +0300

    seminar-11

--- wp-content/themes/chikurov_2020/index.php
+++ wp-content/themes/chikurov_2020/index.php
@@ -154,7 +154,7 @@
           <h2><?php the_title(); ?></h2>
         </div>
         <div class="col-md-6 seminar_type"><i class="fa fa-bookmark" aria-hidden="true"> </i> <?php the_field('seminar_type'); ?> <span class="seminar_timing">
-          <?php if (get_field('online_course')){ ?>
+          <?php if (is_page(6998)){ ?>
             <i class="fa fa-calendar" aria-hidden="true"> </i> 
           <?php } else { ?>
             <i class="fa fa-clock-o" aria-hidden="true"> </i> 
