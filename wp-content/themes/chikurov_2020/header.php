45ee85c9d21cf075c839e8abf5fb7c7ba67af99d
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-29 15:15:36 +0300

    pricing-4

--- wp-content/themes/chikurov_2020/header.php
+++ wp-content/themes/chikurov_2020/header.php
@@ -7,6 +7,17 @@
   <meta name="author" content="">
   <meta name="yandex-verification" content="8bf323184bf04b23" />
   <title><?php the_title(); ?></title>
+  <!-- Google Analytics -->
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-XXXXX-Y', 'auto');
+  ga('send', 'pageview');
+  </script>
+  <!-- End Google Analytics -->]
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap" rel="stylesheet">
