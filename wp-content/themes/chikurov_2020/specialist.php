d136f34fff0e1cd07b0b0d9c2363c7d5fa557de5
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-09-27 12:30:50 +0300

    specilists-9

--- wp-content/themes/chikurov_2020/specialist.php
+++ wp-content/themes/chikurov_2020/specialist.php
@@ -16,6 +16,7 @@ $specilist_phone = get_field('specilist_phone');
 $specilist_phone_alt = get_field('specilist_phone_alt');
 $specilist_email = get_field('specilist_email');
 $specilist_country = get_field('specilist_country');
+$specilist_city = get_field('specilist_city');
 
 echo '<div class="specilist_item col-md-3 col-sm-4 col-xs-12">';
 echo '<img src="';
@@ -26,6 +27,8 @@ echo the_title();
 echo '</h3>';
 echo '<h4>';
 echo $specilist_country;
+echo '</br>';
+echo $specilist_city;
 echo '</h4>';
 echo '<a class="specilist_tel" href="tel:';
 echo $specilist_phone_alt;
