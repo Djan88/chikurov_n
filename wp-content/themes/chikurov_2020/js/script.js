bbc09cc985a0a3794388e3c7e95def2243d0c50e
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-10-01 14:44:31 +0300

    specilists-37

--- wp-content/themes/chikurov_2020/js/script.js
+++ wp-content/themes/chikurov_2020/js/script.js
@@ -129,7 +129,7 @@ jQuery(document).ready(function () {
     jQuery('.modal_specialist_title').text(jQuery(this).find('h3').text());
     jQuery('.modal_specialist_foto').attr('src', jQuery(this).find('img').attr('src'));
     jQuery('.modal_specialist_contacts_adress').text(jQuery(this).find('.adress_specialist_combined').text());
-    jQuery('.modal_specialist_contacts_tel').attr('href', jQuery(this).find('specilist_tel').attr('href'));
+    jQuery('.modal_specialist_contacts_tel').attr('href', jQuery(this).find('.specilist_tel').attr('href'));
     jQuery('.modal_specialist_contacts_tel').text(jQuery(this).find('.specilist_tel').text());
   });
 
