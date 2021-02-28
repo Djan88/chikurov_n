b2c77c2c5ac4bfd9ab5934bf54cfefc8f167192b
Ivan Avraamov <ivan.avraamov@gmail.com>
2021-02-28 10:37:08 +0300

    chain_list-4

--- wp-content/themes/chikurov_2020/single.php
+++ wp-content/themes/chikurov_2020/single.php
@@ -207,7 +207,7 @@
               <?php if (get_field('order_closed')) { ?>
                 <div class="order_closed_text">
                   Сожалеем, но запись на этот семинар уже закрыта.<br>
-                  <div class="seminar_else_text">Клкините по <a href="#seminars_chain" class="else_seminars_link chain_list_link">этой ссылке</a> для записи в лист ожидания (если место освободится мы дадим вам знать в первую очередь) или по <a href="#seminars_else" class="else_seminars_link">этой ссылке</a> и ознакомьтесь с расписанием следующих семинаров "<?php the_title(); ?>"</div>
+                  <div class="seminar_else_text">Клкините по <a href="#seminars_chain" class="else_seminars_link chain_list_link">этой ссылке</a> для записи в лист ожидания (<i>если место освободится мы дадим вам знать в первую очередь</i>) или по <a href="#seminars_else" class="else_seminars_link">этой ссылке</a> и ознакомьтесь с расписанием следующих семинаров "<?php the_title(); ?>"</div>
                   <div id="seminars_chain">
                     <div class="chain_list hidden">
                       <h5 class="content_center" id="chain">Лист ожидания</h5>
