9c32f748357e5546af357db19b90a9cd97977b83
Ivan Avraamov <ivan.avraamov@gmail.com>
2022-02-03 18:57:16 +0300

    socials_4

--- wp-content/themes/chikurov_2020/header.php
+++ wp-content/themes/chikurov_2020/header.php
@@ -78,11 +78,11 @@
         </div>
         <div class="col-xs-12 col-sm-3 col-md-3 header_contacts pull-right">
           <div class="row">
-            <div class="col-xs-12 col-sm-10 col-md-10">
+            <div class="col-xs-12 col-sm-8 col-md-8">
               <a class="menu menu_tel" href="tel:+79855381468">+7 (985) 538-14-68</a>
               <a class="menu menu_email" href="mailto:info@chikurov.com">info@chikurov.com</a>
             </div>
-            <div class="col-xs-12 col-sm-2 col-md-2">
+            <div class="col-xs-12 col-sm-4 col-md-4">
               <div class="socials">
                 <a href="https://telegram.me/name_of_domain" class="socials_link socials_link_tg"><i class="fa fa-telegram"></i></a>
                 <a href="https://api.whatsapp.com/send?phone=79855381468" target="_blank" class="socials_link socials_link_wh"><i class="fa fa-whatsapp"></i></a>
